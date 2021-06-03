4f2902ecb18606688bc97857454ad9ad24e706d1
tootootooyaman <toyama.a.ab@gmail.com>
2021-06-03 23:27:30 +0900

    firebase.jsを追加、firebaseConfigを設定

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase"
+import "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAkHcq4QYhsusFyALSjn5euj0G4kdDHvPo",
+  authDomain: "new-mentor-practice.firebaseapp.com",
+  projectId: "new-mentor-practice",
+  storageBucket: "new-mentor-practice.appspot.com",
+  messagingSenderId: "149318117393",
+  appId: "1:149318117393:web:c58aadaa4173da6ed329b0",
+  measurementId: "G-HKGJLBN2DV",
+}
+
+firebase.initializeApp(firebaseConfig)
